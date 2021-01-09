620871d68a936be011b3830af297a56b648bf3d1
amin bashir <aminbashir65@gmail.com>
2021-01-09 14:08:20 -0600

    fixed character

--- script.js
+++ script.js
@@ -10,51 +10,60 @@ var symbolsArray = "!@#$%^&*+_-".split("")
 
 function generatePassword() {
     var length = prompt("Choose a number between 8 to 128");
-    var lowercase = confirm("Do you want lowercase letters");
-    var uppercase = confirm("Do you want uppercase letters");
-    var numbers = confirm("Do you want numbers");
-    var symbols = confirm("Do you want special characters");
+    if (length < 8 || length > 128) {
 
-    if (lowercase === false && uppercase === false && numbers === false && symbols === false) {
-        alert(" please select atlease one Character type");
-        return;
-    }
+        alert("chosen number does not fit criteria")
+        generatePassword()
+    } else {
 
-    var availableCharacters = []
-    var result = ""
-    var chosenChar = 1
 
-    if (lowercase === true) {
-        availableCharacters = lowerArray.concat(availableCharacters)
-        var lower = randomChar(lowerArray)
-        result = lower + result
-        chosenChar = chosenChar + 1
+        var lowercase = confirm("Do you want lowercase letters");
+        var uppercase = confirm("Do you want uppercase letters");
+        var numbers = confirm("Do you want numbers");
+        var symbols = confirm("Do you want special characters");
+
+        if (lowercase === false && uppercase === false && numbers === false && symbols === false) {
+            alert(" please select atlease one Character type");
+            return;
+        }
+
+        var availableCharacters = []
+        var result = ""
+        var chosenChar = 1
+
+        if (lowercase === true) {
+            availableCharacters = lowerArray.concat(availableCharacters)
+            var lower = randomChar(lowerArray)
+            result = lower + result
+            chosenChar = chosenChar + 1
+        }
+        if (uppercase === true) {
+            availableCharacters = upperArray.concat(availableCharacters)
+            var upper = randomChar(upperArray)
+            result = upper + result
+            chosenChar = chosenChar + 1
+        }
+        if (numbers === true) {
+            availableCharacters = numbersArray.concat(availableCharacters)
+            var number = randomChar(numbersArray)
+            result = number + result
+            chosenChar = chosenChar + 1
+        }
+        if (symbols === true) {
+            availableCharacters = symbolsArray.concat(availableCharacters)
+            var symbol = randomChar(symbolsArray)
+            result = symbol + result
+            chosenChar = chosenChar + 1
+        }
+        //console.log(chosenChar)
+        for (var i = 0; i <= length; i++) {
+            var char = randomChar(availableCharacters)
+            result = char + result
+            console.log(result)
+        }
+        result = result.slice(chosenChar, result.length);
+        return result
     }
-    if (uppercase === true) {
-        availableCharacters = upperArray.concat(availableCharacters)
-        var upper = randomChar(upperArray)
-        result = upper + result
-        chosenChar = chosenChar + 1
-    }
-    if (numbers === true) {
-        availableCharacters = numbersArray.concat(availableCharacters)
-        var number = randomChar(numbersArray)
-        result = number + result
-        chosenChar = chosenChar + 1
-    }
-    if (symbols === true) {
-        availableCharacters = symbolsArray.concat(availableCharacters)
-        var symbol = randomChar(symbolsArray)
-        result = symbol + result
-        chosenChar = chosenChar + 1
-    }
-    //console.log(chosenChar)
-    for (var i = 0; i <= length; i++) {
-        var char = randomChar(availableCharacters)
-        result = char + result
-    }
-    result = result.slice(chosenChar, result.length);
-    return result
 }
 
 function randomChar(array) {
